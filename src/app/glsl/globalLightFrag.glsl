4836f757ceb71bf94eb9157a6988f4994ac7ca37
WillCS <wcstibbards@gmail.com>
2018-07-18 00:51:33 +1000

    Changed shaders to high precision floating points

--- src/app/glsl/globalLightFrag.glsl
+++ src/app/glsl/globalLightFrag.glsl
@@ -1,4 +1,4 @@
-precision mediump float;
+precision highp float;
 
 #define MAX_LIGHTS 1
 
--- src/app/glsl/globalLightVert.glsl
+++ src/app/glsl/globalLightVert.glsl
@@ -1,4 +1,4 @@
-precision mediump float;
+precision highp float;
 
 attribute vec4 vertexPos;
 attribute vec4 vertexColour;
